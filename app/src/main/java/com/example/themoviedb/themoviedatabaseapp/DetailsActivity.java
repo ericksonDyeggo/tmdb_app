0801fb2901035fc3bd2c73fb30750522d7359179
Erickson Takano <ericksondyeggo@gmail.com>
2017-02-26 20:10:47 -0300

    Add feature movie details

--- app/src/main/java/com/example/themoviedb/themoviedatabaseapp/DetailsActivity.java
+++ app/src/main/java/com/example/themoviedb/themoviedatabaseapp/DetailsActivity.java
@@ -2,16 +2,18 @@ package com.example.themoviedb.themoviedatabaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.themoviedb.themoviedatabaseapp.model.Movie;
 import com.squareup.picasso.Picasso;
 
-import org.androidannotations.annotations.AfterExtras;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.ViewById;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created by erickson on 26/02/17.
@@ -34,24 +36,56 @@ public class DetailsActivity extends AppCompatActivity {
     @ViewById
     ImageView moviePosterImageView;
 
+    @ViewById
+    RatingBar voteAverageRatingRatingBar;
+
     @Extra
     Movie movie;
 
     @AfterViews
     void init() {
-        movieOriginalTitleTextView.setText(movie.getOriginalTitle());
+        setMovieTitle();
 
-        movieTitleTextView.setText(movie.getTitle());
+        setSynopsis();
 
-        movieSynopsisTextView.setText(movie.getSynopsis());
+        setReleaseDate();
 
-        movieReleaseDateTextView.setText(movie.getReleaseDate().toString());
+        setMoviePoster();
 
+        setVoteAverage();
+    }
+
+    private void setVoteAverage() {
+        voteAverageRatingRatingBar.setRating((movie.getVoteAverage() / 2));
+    }
+
+    private void setMoviePoster() {
         Picasso.with(this)
-                .load(movie.getPosterPath())
+                .load(movie.getPosterPath(Movie.W780))
                 .placeholder(R.mipmap.timthumb)
                 .error(R.mipmap.timthumb)
                 .noFade()
                 .into(moviePosterImageView);
+    }
+
+    private void setReleaseDate() {
+        SimpleDateFormat sdf =
+                ((SimpleDateFormat) android.text.format.DateFormat.getDateFormat(this));
+
+        movieReleaseDateTextView.setText(sdf.format(movie.getReleaseDate()));
+    }
+
+    private void setMovieTitle() {
+        movieOriginalTitleTextView.setText(movie.getOriginalTitle());
+
+        movieTitleTextView.setText(movie.getTitle());
+    }
+
+    private void setSynopsis() {
+        movieSynopsisTextView.setText(
+                movie.getSynopsis().isEmpty() ?
+                        getString(R.string.no_overview) :
+                        movie.getSynopsis()
+        );
     }
 }
--- app/src/main/java/com/example/themoviedb/themoviedatabaseapp/model/Movie.java
+++ app/src/main/java/com/example/themoviedb/themoviedatabaseapp/model/Movie.java
@@ -13,6 +13,15 @@ public class Movie implements Serializable {
 
     private final static String IMG_URL = "https://image.tmdb.org/t/p/%s%s";
 
+    public final static int W92 = 1;
+    public final static int W154 = 2;
+    public final static int W185 = 3;
+    public final static int W342 = 4;
+    public final static int W500 = 5;
+    public final static int W780 = 6;
+    public final static int ORIGINAL = 0;
+    private final String[] sizes = {"original", "w92", "w154", "w185", "w342", "w500", "w780"};
+
     private Long id;
     @SerializedName("original_title")
     private String originalTitle;
@@ -24,7 +33,7 @@ public class Movie implements Serializable {
     @SerializedName("release_date")
     private Date releaseDate;
     @SerializedName("vote_average")
-    private Double voteAverage;
+    private float voteAverage;
 
     public Long getId() {
         return id;
@@ -50,8 +59,8 @@ public class Movie implements Serializable {
         this.title = title;
     }
 
-    public String getPosterPath() {
-        String posterURL = String.format(IMG_URL, "w780", posterPath);
+    public String getPosterPath(int size) {
+        String posterURL = String.format(IMG_URL, sizes[size], posterPath);
         return posterURL;
     }
 
@@ -75,11 +84,11 @@ public class Movie implements Serializable {
         this.releaseDate = releaseDate;
     }
 
-    public Double getVoteAverage() {
+    public float getVoteAverage() {
         return voteAverage;
     }
 
-    public void setVoteAverage(Double voteAverage) {
+    public void setVoteAverage(float voteAverage) {
         this.voteAverage = voteAverage;
     }
 }
